e756438a399570377123b574afca024ec87c61d1
Max Sydney <maxsydney8@gmail.com>
2021-11-16 23:18:37 +1300

    Wrestling with walletconnect

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "algorand-walletconnect-qrcode-modal";
+// import algosdk from "algosdk";
+// import { formatJsonRpcRequest } from "@json-rpc-tools/utils";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  connectWallet() {
+    // Create a connector
+    const connector = new WalletConnect({
+      bridge: "https://bridge.walletconnect.org", // Required
+      qrcodeModal: QRCodeModal,
+    });
+
+    // Check if connection is already established
+    if (!connector.connected) {
+      // create new session
+      connector.createSession();
+    }
+
+    console.log(connector);
+
+    // Subscribe to connection events
+    connector.on("connect", (error, payload) => {
+      if (error) {
+        throw error;
+      }
+
+      console.log("Connecting");
+      // Get provided accounts
+      const { accounts } = payload.params[0];
+    });
+
+    connector.on("session_update", (error, payload) => {
+      if (error) {
+        throw error;
+      }
+
+      console.log("Update")
+      // Get updated accounts 
+      const { accounts } = payload.params[0];
+    });
+
+    QRCodeModal.open(connector.uri);
+  }
 }
